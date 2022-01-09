4802324d149248005b5e3ea72475facdace6569d
Nicolas Frankel <nicolas@frankel.ch>
2022-01-09 13:19:02 +0200

    Further refactor if conditions

--- src/gildedrose.rs
+++ src/gildedrose.rs
@@ -51,11 +51,11 @@ impl GildedRose {
                         }
                     }
                 }
+            } else if item.name == "Sulfuras, Hand of Ragnaros" {
+
             } else {
                 if item.quality > 0 {
-                    if item.name != "Sulfuras, Hand of Ragnaros" {
-                        item.quality -= 1;
-                    }
+                    item.quality -= 1;
                 }
             }
 
@@ -68,15 +68,13 @@ impl GildedRose {
                     if item.quality < 50 {
                         item.quality += 1;
                     }
+                } else if item.name == "Backstage passes to a TAFKAL80ETC concert" {
+                    item.quality = item.quality - item.quality;
+                } else if item.name == "Sulfuras, Hand of Ragnaros" {
+
                 } else {
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert" {
-                        if item.quality > 0 {
-                            if item.name != "Sulfuras, Hand of Ragnaros" {
-                                item.quality -= 1;
-                            }
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality;
+                    if item.quality > 0 {
+                        item.quality -= 1;
                     }
                 }
             }
